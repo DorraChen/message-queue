0a73c848688d8ec04f35bf8cc99514c2d4d788af
clz <1814786186@qq.com>
2019-09-23 18:16:21 +0800

    2019/09/23 add:RabbitMQ Test

---
+++ commons/src/main/java/com/example/queue/redis/RedisClient.java
@@ -0,0 +1,9 @@
+package com.example.queue.redis;
+
+/**
+ * @author: clz
+ * @date: 2019/09/23 18:11
+ * @description:
+ */
+public class RedisClient {
+}
---
+++ rabbitmq-test/src/main/java/com/example/queue/RabbitmqTestApplication.java
@@ -0,0 +1,17 @@
+package com.example.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: clz
+ * @date: 2019/09/23 18:12
+ * @description:
+ */
+@SpringBootApplication
+public class RabbitmqTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqTestApplication.class, args);
+    }
+
+}
